60bc0b7c9fd97f71b0181d1105683e470efc8d79
muy <muy6324@gmail.com>
2026-05-27 13:46:09 -0400

    chore: update DB credentials for new cloud run environment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEBUG = True
 
 # MySQL 连接：云托管部署时自动注入环境变量，本地开发用下面的默认值
 username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'Dogfan2887')
+password = os.environ.get("MYSQL_PASSWORD", '9CPcksGG')
 db_address = os.environ.get(
     "MYSQL_ADDRESS",
     'sh-cynosdbmysql-grp-3c7a1auu.sql.tencentcdb.com:20699'
